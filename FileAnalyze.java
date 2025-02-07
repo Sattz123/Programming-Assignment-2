5504d0c533377c7301c7201862b115d186321e87
Stewiee <107410522+Sattz123@users.noreply.github.com>
2025-02-07 18:50:48 +0700

    update

--- FileAnalyze.java
+++ FileAnalyze.java
@@ -3,47 +3,45 @@ import java.util.*;
 
 public class FileAnalyze {
     public static void main(String[] args) {
-        long start = System.currentTimeMillis();
         int charCounter = 0;
         int palindromeCounter = 0;
         int tokenCounter = 0;
         int newLineCounter = 0;
         int longestToken = 0;
-        int totalToken = 0;
+        int totalTokenLength = 0;
         int emoticonCounter = 0;
-        
-        List<String> tokens = new ArrayList<>();
+
         List<String> emoticons = Arrays.asList(":)", ":(", ":D", ";)", ":P", "XD");
-        
+
         try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Stewie\\Documents\\ADT Programming Assignment 2\\Java File\\Programming Assignment 2\\src\\input1.txt"))) {
             String line;
             while ((line = br.readLine()) != null) {
                 charCounter += line.length();
                 newLineCounter++;
-                
-                String[] words = line.split("\\s+");
+
+                String[] words = line.split("[^a-zA-Z0-9]+");
                 for (String word : words) {
-                    tokens.add(word);
-                    tokenCounter++;
-                    totalToken += word.length();
-                    if (word.length() > longestToken) {
-                        longestToken = word.length();
-                    }
-                    if (isPalindrome(word)) {
-                        palindromeCounter++;
-                    }
-                    if (emoticons.contains(word)) {
-                        emoticonCounter++;
+                    if (!word.isEmpty()) {
+                        tokenCounter++;
+                        totalTokenLength += word.length();
+                        if (word.length() > longestToken) {
+                            longestToken = word.length();
+                        }
+                        if (isPalindrome(word)) {
+                            palindromeCounter++;
+                        }
+                        if (emoticons.contains(word)) {
+                            emoticonCounter++;
+                        }
                     }
                 }
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Error reading the file: " + e.getMessage());
         }
-        
-        double averageTokenSize = (double) totalToken / tokenCounter;
 
-        
+        double averageTokenSize = tokenCounter > 0 ? (double) totalTokenLength / tokenCounter : 0;
+
         System.out.println("Program start:");
         System.out.println("Total character count: " + charCounter);
         System.out.println("Total palindrome count: " + palindromeCounter);
@@ -52,22 +50,16 @@ public class FileAnalyze {
         System.out.println("Total number of new lines: " + newLineCounter);
         System.out.println("Longest token size: " + longestToken);
         System.out.println("Average token size: " + averageTokenSize);
-        long end = System.currentTimeMillis();
-        double totalTime = (end - start) / 1000.0;
-        System.out.println("Execution time: " + totalTime + " seconds");
     }
-    
+
     private static boolean isPalindrome(String word) {
-        int left = 0;
-        int right = word.length() - 1;
-    
+        word = word.toLowerCase();
+        int left = 0, right = word.length() - 1;
         while (left < right) {
-            if (word.charAt(left) != word.charAt(right)) {
+            if (word.charAt(left++) != word.charAt(right--)) {
                 return false;
             }
-            left++;
-            right--;
         }
-        return true;
+        return word.length() > 1;
     }
 }
